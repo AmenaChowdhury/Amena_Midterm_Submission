3e40114e11fa4f180899e94f51a20b5e0e1b123f
AmenaChowdhury <amchdata@gmail.com>
2018-12-23 22:01:56 -0500

    String and so on

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -1,8 +1,5 @@
 package string.problems;
 
-/**
- * Created by mrahman on 04/22/17.
- */
 public class Permutation {
 
     public static void main(String[] args) {
@@ -12,5 +9,17 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        permutation("", "efgh");
+    }
+    public static void permutation(String prefix, String sg){
+        int n = sg.length();
+
+        if(n==0){
+            System.out.println(prefix);
+        }else{
+            for(int i=0; i < n; i++){
+                permutation(prefix + sg.charAt(i), sg.substring(0, i) + sg.substring(i+1,n));
+            }
+        }
     }
 }
